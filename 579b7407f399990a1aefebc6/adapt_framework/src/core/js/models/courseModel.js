ceac5801b182cc894aeb1f1716ed659391e0c99a
root <root@create.learndata.info>
2016-11-28 05:57:56 -0500

    Auto backup bot

---
+++ 579b7407f399990a1aefebc6/adapt_framework/src/core/js/models/courseModel.js
@@ -0,0 +1,30 @@
+define(function(require) {
+
+    var AdaptModel = require('coreModels/adaptModel');
+    var Adapt = require('coreJS/adapt');
+
+    var CourseModel = AdaptModel.extend({
+
+        initialize: function(attrs, options) {
+            AdaptModel.prototype.initialize.apply(this, arguments);
+            Adapt.trigger('courseModel:dataLoading');
+
+            this.url = options.url;
+
+            this.on('sync', this.loadedData, this);
+            if (this.url) {
+                this.fetch();
+            }
+        },
+
+        loadedData: function() {
+            Adapt.trigger('courseModel:dataLoaded');
+        },
+
+        _children: "contentObjects"
+
+    });
+
+    return CourseModel;
+
+});
---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mongodump --db adapt-tenant-master -o adapt-tenant-master
+git add *
+git commit -a -m "Auto backup bot"
+git push origin master
